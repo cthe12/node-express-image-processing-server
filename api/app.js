49d1b3ff60b226f5e82ee45ec6f32c4aa3af21d3
Chantel T <chantel.theodule@outlook.com>
2022-04-02 13:08:20 -0400

    created image upload page

---
+++ api/app.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const { use } = require("express/lib/application")
+const { sendFile } = require("express/lib/response")
+const path = require("path")
+const app = express()
+const pathToIndex = '../client/index.html'
+path.resolve(__dirname, '../client/index.html') = pathToIndex
+use('/*', (req, res) => {
+    res.sendFile(pathToIndex)
+})
+module.exports(app)

0957e04c0766422e0957fa907bb8c9abf88909bf
Chantel T <chantel.theodule@outlook.com>
2022-04-03 15:07:43 -0400

    syntax update

--- api/app.js
+++ api/app.js
@@ -5,7 +5,7 @@ const path = require("path")
 const app = express()
 const pathToIndex = '../client/index.html'
 path.resolve(__dirname, '../client/index.html') = pathToIndex
-use('/*', (req, res) => {
+app.use('/*', (req, res) => {
     res.sendFile(pathToIndex)
 })
 module.exports(app)

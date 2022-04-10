bd68f3291a5b490665cf499b466d7702b5600f66
Chantel T <chantel.theodule@outlook.com>
2022-04-10 17:16:06 -0400

    form response

--- api/app.js
+++ api/app.js
@@ -3,9 +3,8 @@ const { use } = require("express/lib/application")
 const { sendFile } = require("express/lib/response")
 const path = require("path")
 const app = express()
-const pathToIndex = '../client/index.html'
-path.resolve(__dirname, '../client/index.html') = pathToIndex
+const pathToIndex = path.resolve(__dirname, '../client/index.html')
 app.use('/*', (req, res) => {
     res.sendFile(pathToIndex)
 })
-module.exports(app)
+module.exports = app

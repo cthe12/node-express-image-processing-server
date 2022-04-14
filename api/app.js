671051dfbcfcb20fa4dd2dfb8c5c5a00a5420a62
Chantel T <chantel.theodule@outlook.com>
2022-04-13 23:11:06 -0400

    wire up router

--- api/app.js
+++ api/app.js
@@ -1,13 +1,16 @@
-const express = require("express")
-const { use } = require("express/lib/application")
-const { sendFile } = require("express/lib/response")
-const path = require("path")
-const router = require("./src/router")
-const app = express()
-const pathToIndex = path.resolve(__dirname, '../client/index.html')
-app.use("/", router)
-app.use(express.static(path.resolve(__dirname, "uploads")))
-app.use('/*', (req, res) => {
-    res.sendFile(pathToIndex)
-})
-module.exports = app
+const express = require('express');
+const path = require('path');
+const router = require('./src/router');
+
+const app = express();
+
+const pathToIndex = path.resolve(__dirname, '../client/index.html');
+
+app.use('/', router);
+
+app.use(express.static(path.resolve(__dirname, 'uploads')));
+app.use('/*', (request, response) => {
+  response.sendFile(pathToIndex);
+});
+
+module.exports = app;
